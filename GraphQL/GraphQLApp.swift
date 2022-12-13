4853cad6c9e18e03f1279dfb5ace6fd2adeb8bc3
Camelia Braghes <cameliabraghes@gmail.com>
2022-12-13 14:49:00 +0200

    Add load from file and json file

--- GraphQL/GraphQLApp.swift
+++ GraphQL/GraphQLApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct GraphQLApp: App {
+    @State var launchViewModel = LaunchViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(launchViewModel)
         }
     }
 }

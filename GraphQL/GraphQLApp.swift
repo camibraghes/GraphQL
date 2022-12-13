62394eb349d5035c7736c6bd7030853c501a7e19
Camelia Braghes <cameliabraghes@gmail.com>
2022-12-13 13:52:10 +0200

    Initial commit

---
+++ GraphQL/GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLApp.swift
+//  GraphQL
+//
+//  Created by Camelia Braghes on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
